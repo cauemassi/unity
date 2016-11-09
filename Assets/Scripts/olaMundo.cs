1bc25b0324e1f23e5193d06c4f5073dcc1d134a7
Davi3760 <davi3760@hotmail.com>
2016-11-09 18:21:58 -0200

    primeiro commit do Dave

---
+++ Assets/Scripts/olaMundo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class olaMundo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("ola mundo");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
